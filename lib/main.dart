c67eb337e15e930c74ad165b26200ddff503cdf7
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-10-28 00:22:29 -0500

    Widgets with containershave been created

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            'Hello !',
+            style: TextStyle(fontSize: 40.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello rectangle'),
+        ),
+        body: HelloRectangle(),
+      ),
+    ),
+  );
+}
